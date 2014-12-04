15f9d64b8fb4c84b41d18437b028ed4ebbee62f9
Xuefeng-Zhu <xzhu15@illinois.edu>
2014-12-04 08:02:57 -0600

    process_data_by_admit

---
+++ data/process_data_by_admit.py
@@ -0,0 +1,33 @@
+import json
+# Unique_key, department_name, term, year, gre_quant, gre_verbal, gre_awa, toefl, undergrad_gpa
+def process_data(data, index):
+	result = {}
+	for individual in data:
+		score = int(individual[index])
+		if index != 6:
+			tmp = score / 5 * 5
+		else:
+			tmp = score
+			
+		if result.get(score):
+			result[tmp] += 1
+		else:
+			result[tmp] = 1
+	return result 
+
+if __name__ == '__main__':
+	metrics = {'gre_quant': 4, 'gre_verbal': 5, 'gre_awa': 6, 'toefl': 7, 'gpa': 8}
+	with open('uni_training.json') as f:
+		data = json.load(f)
+	universities = {}
+	for university in data:
+		admit = data[university]['admit']
+		reject = data[university]['reject']
+		tmp = {}
+		for metric in metrics:
+			tmp[metric] = {}
+			tmp[metric]['admit'] = process_data(admit, metrics[metric])
+			tmp[metric]['reject'] = process_data(reject, metrics[metric])
+		universities[university.split('/')[0].title()] = tmp 
+	with open('data_by_admit.json', 'w') as f:
+		json.dump(universities, f)
--- demo/js/controllers.js
+++ demo/js/controllers.js
@@ -5,90 +5,98 @@ google.load("visualization", "1", {
 
 /* Controllers */
 var url = 'https://back-school.firebaseio.com/'
-angular.module('myApp.controllers', []).
-controller('ResearchCtrl', ['$scope', '$http',
-    function($scope, $http) {
-        $scope.universities = universities;
-        $scope.metrics = metrics;
-        $scope.comparisons = []
+angular.module('myApp.controllers', [])
+    .controller('MainCtrl', ['$scope', '$http',
+        function($scope, $http) {
+            $scope.universities = universities;
+            $scope.metrics = metrics;
+        }
+    ])
+    .controller('ResearchCtrl', ['$scope', '$http',
+        function($scope, $http) {
+            $scope.comparisons = []
 
-        $scope.addNewComp = function() {
-            var ref = $scope.comparisons
-            var size = ref.length;
-            ref.push({
-                id: size,
-                university: undefined,
-                metric: $scope.metric,
-                data: undefined
-            })
+            $scope.addNewComp = function() {
+                var ref = $scope.comparisons
+                var size = ref.length;
+                ref.push({
+                    id: size,
+                    university: undefined,
+                    metric: $scope.metric,
+                    data: undefined
+                })
 
-            // initiate data 
-            var data = [
-                ['Year']
-            ];
-            for (var i = 2006; i < 2015; i++) {
-                data.push(['' + i]);
+                // initiate data 
+                var data = [
+                    ['Year']
+                ];
+                for (var i = 2006; i < 2015; i++) {
+                    data.push(['' + i]);
+                }
+
+                ref[size].data = data;
+
+                $http.get([url, 'years', $scope.university + '.json'].join('/'))
+                    .success(function(response) {
+                        processRes(response, ref[size], $scope.university);
+                        plotChart(ref[size]);
+                        $scope.university = null;
+                        $scope.metric = null;
+                    });
             }
 
-            ref[size].data = data;
+            $scope.addNewUniversity = function(ref) {
+                $http.get([url, 'years', ref.university + '.json'].join('/'))
+                    .success(function(response) {
+                        processRes(response, ref, ref.university);
+                        plotChart(ref);
+                        ref.university = null;
+                    });
+            }
 
-            $http.get([url, 'years', $scope.university + '.json'].join('/'))
-                .success(function(response) {
-                    processRes(response, ref[size], $scope.university);
-                    plotChart(ref[size]);
-                    $scope.university = null;
-                    $scope.metric = null;
-                });
-        }
+            function processRes(res, ref, university) {
+                var metric = ref.metric;
+                var data = ref.data;
+                var prev = null;
 
-        $scope.addNewUniversity = function(ref) {
-            $http.get([url, 'years', ref.university + '.json'].join('/'))
-                .success(function(response) {
-                    processRes(response, ref, ref.university);
-                    plotChart(ref);
-                    ref.university = null;
-                });
-        }
+                data[0].push(university);
 
-        function processRes(res, ref, university) {
-            var metric = ref.metric;
-            var data = ref.data;
-            var prev = null;
+                for (var year = 2006; year < 2015; year++) {
+                    if (res[year]) {
+                        var individuals = res[year];
+                        var sum = 0;
+                        for (var i in individuals) {
+                            sum += individuals[i][metric];
+                        }
+                        prev = Math.round(sum / individuals.length)
+                        data[year - 2005].push(prev);
 
-            data[0].push(university);
-
-            for (var year = 2006; year < 2015; year++) {
-                if (res[year]) {
-                    var individuals = res[year];
-                    var sum = 0;
-                    for (var i in individuals) {
-                        sum += individuals[i][metric];
+                    } else {
+                        data[year - 2005].push(prev);
                     }
-                    prev = Math.round(sum / individuals.length)
-                    data[year - 2005].push(prev);
-
-                } else {
-                    data[year - 2005].push(prev);
                 }
             }
+
+            function plotChart(ref) {
+                var data = google.visualization.arrayToDataTable(ref.data);
+                var options = {
+                    title: 'Comparison between Universities on ' + ref.metric,
+                    curveType: 'function',
+                };
+                var chart = new google.visualization.LineChart(document.getElementById('chart_div' + ref.id));
+
+                chart.draw(data, options);
+            }
+
         }
-
-        function plotChart(ref) {
-            var data = google.visualization.arrayToDataTable(ref.data);
-            var options = {
-                title: 'Comparison between Universities on ' + ref.metric,
-                curveType: 'function',
-            };
-            var chart = new google.visualization.LineChart(document.getElementById('chart_div' + ref.id));
-
-            chart.draw(data, options);
-        }
-
-    }
-])
+    ])
     .controller('SearchCtrl', ['$scope', '$http',
-    function($scope, $http){
-    	$scope.universities = universities;
-
-    }
+        function($scope, $http) {
+        	$scope.search = function(){
+        		$http.get([url, 'scatter', $scope.university, $scope.metric + '.json'].join('/'))
+        			.success(function(response){
+        				var data = processRes(response);
+        			});
+        	}
+        }
     ]);
