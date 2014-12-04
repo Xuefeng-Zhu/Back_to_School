3ae2bde41a4191ca52ff028295361fe5b27f624a
Xuefeng-Zhu <xzhu15@illinois.edu>
2014-12-04 02:31:26 -0600

    add new compa

--- demo/js/app.js
+++ demo/js/app.js
@@ -7,7 +7,11 @@ angular.module('myApp', [
   'myApp.controllers'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
+  $routeProvider.when('/research', 
+  	{templateUrl: 'partials/research_universities.html', controller: 'ResearchCtrl'});
   $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider.otherwise({redirectTo: '/research'});
 }]);
+
+var universities = ["polytechnic university of new york", "university of washington", "purdue university", "university of rochester", "rensselaer polytechnic institute", "new york institute of technology", "university of nebraska omaha", "university of virginia", "stevens institute of technology", "university of central florida", "university of maryland college park", "georgia institute of technology", "new jersey institute of technology", "university of south florida", "florida institute of technology", "michigan technological university", "new york university", "illinois institute of technology", "kansas state university", "university of minnesota twin cities", "university of illinois chicago", "suny college at buffalo (buffalo state college)", "university of utah", "suny albany", "rutgers university", "university of florida", "university of wisconsin madison", "indiana university-bloomington", "rochester institute of technology", "george mason university", "michigan state university", "columbia university", "university of michigan ann arbor", "university of illinois springfield", "syracuse university", "university of maryland baltimore county", "university of colorado boulder", "university of illinois urbana-champaign", "wayne state university", "johns hopkins university", "cornell university", "suny binghamton", "university of kansas", "florida state university", "utah state university", "george washington university virginia campus", "oregon state university", "suny stony brook", "wichita state university", "colorado state university", "indiana university", "northwestern university", "virginia polytechnic institute and state university", "clemson university", "washington state university", "georgia state university", "portland state university", "northern illinois university", "suny buffalo", "florida international university", "george washington university at mount vernon college"]
+var metrics = ["gre_quant", "gre_verbal", "gre_awa", "toefl", "undergrad_gpa"]
--- demo/js/controllers.js
+++ demo/js/controllers.js
@@ -1,11 +1,51 @@
 'use strict';
 
 /* Controllers */
-
+var url = 'https://back-school.firebaseio.com/'
 angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
+controller('ResearchCtrl', ['$scope', '$http',
+    function($scope, $http) {
+        $scope.universities = universities;
+        $scope.metrics = metrics;
+        $scope.comparisons = []
 
-  }])
-  .controller('MyCtrl2', [function() {
+        $scope.addNewComp = function() {
+        	var ref = $scope.comparisons
+        	var size = ref.length;
+            ref.push({
+                id: size,
+                university: undefined,
+                metric: $scope.metric,
+                data: undefined
+            })
 
-  }]);
+            // initiate data 
+            var data = [['Year']];
+            for (var i = 2006; i < 2015; i++){
+            	data.push(['' + i]);
+            }
+
+            ref[size].data = data;
+
+            $http.get([url, 'years', $scope.university + '.json'].join('/'))
+                .success(function(response) {
+                	var data = ref[size].data;
+
+
+
+            })
+        }
+
+        function processRes(res, ref){
+        	var metric = ref.metric;
+        	var data = ref.data;
+        	
+        }
+
+    }
+])
+    .controller('MyCtrl2', [
+        function() {
+
+        }
+    ]);
